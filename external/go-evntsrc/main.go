06743964eb2574aa1c9b8138ad6458af6e3478e6
Tom Worrall <tom@hostngo.com.au>
2019-03-18 13:35:54 +1100

    feat: go client

---
+++ external/go-evntsrc/main.go
@@ -0,0 +1,178 @@
+package evntsrc
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/tcfw/evntsrc/pkg/event"
+	"github.com/tcfw/evntsrc/pkg/websocks"
+)
+
+const (
+	apiEndpoint        = "evntsrc.io"
+	stagingAPIEndpoint = "staging.evntsrc.io"
+	apiVersion         = "v1"
+	endpointAPI        = "api"
+	endpointIngress    = "ingress"
+)
+
+//APIClient the basic struct for the api
+type APIClient struct {
+	auth          string
+	stream        int32
+	token         string
+	Endpoint      string
+	httpClient    *http.Client
+	socket        *websocket.Conn
+	connectionID  string
+	AppID         string
+	AppVer        string
+	acks          map[string]*ackCT
+	subscriptions map[string][]*subscription
+
+	ReadPipe  chan []byte
+	writePipe chan *websocks.PublishCommand
+	close     chan bool
+	Errors    chan error
+	AcksCh    chan *websocks.AckCommand
+	ackL      sync.RWMutex
+
+	//Ignore events we published
+	IgnoreSelf bool
+}
+
+//NewEvntSrcClient creates a new client instance for interacting with evntsrc.io
+func NewEvntSrcClient(auth string, streamID int32) (*APIClient, error) {
+	api := &APIClient{
+		auth:          auth,
+		stream:        streamID,
+		Endpoint:      apiEndpoint,
+		httpClient:    newHTTPClient(),
+		ReadPipe:      make(chan []byte, 256),
+		writePipe:     make(chan *websocks.PublishCommand, 256),
+		close:         make(chan bool, 1),
+		Errors:        make(chan error, 256),
+		AcksCh:        make(chan *websocks.AckCommand, 256),
+		AppVer:        "0.1",
+		acks:          map[string]*ackCT{},
+		subscriptions: map[string][]*subscription{},
+		IgnoreSelf:    true,
+	}
+
+	go api.watchAcks()
+
+	return api, nil
+}
+
+type ackCT struct {
+	ts  time.Time
+	ack *websocks.AckCommand
+}
+
+type subscriptionType string
+
+const (
+	funcSubType = "func"
+	chanSubType = "chan"
+)
+
+//Event is the main structure of an event
+type Event = event.Event
+
+type subscription struct {
+	subType subscriptionType
+	ch      chan *Event
+	f       func(*Event)
+}
+
+func newHTTPClient() *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			IdleConnTimeout: 15 * time.Second,
+			MaxIdleConns:    10,
+		},
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return nil
+		},
+	}
+}
+
+func (api *APIClient) watchAcks() {
+	gci := time.Tick(1 * time.Minute)
+	for {
+		select {
+		case <-gci:
+			go api.gcAcks()
+			break
+		case ack := <-api.AcksCh:
+			if ack.Ref != "" {
+				api.ackL.Lock()
+				api.acks[ack.Ref] = &ackCT{ts: time.Now(), ack: ack}
+				api.ackL.Unlock()
+			}
+			break
+		}
+	}
+}
+
+func (api *APIClient) gcAcks() {
+	defer func() {
+		api.ackL.Unlock()
+	}()
+	api.ackL.Lock()
+	tmo := time.Now().Add(-time.Minute)
+	for ref, act := range api.acks {
+		if act.ts.Before(tmo) {
+			delete(api.acks, ref)
+		}
+	}
+}
+
+func (api *APIClient) waitForResponse(cmdRef string) (bool, error) {
+	var vFerr error
+	vFound := make(chan bool, 1)
+	go func() {
+		for {
+			api.ackL.RLock()
+			if ack, ok := api.acks[cmdRef]; ok {
+				api.ackL.RUnlock()
+				ret := ack.ack.Acktype == "OK"
+				if ack.ack.Error != "" {
+					vFerr = errors.New(ack.ack.Error)
+				}
+				vFound <- ret
+				return
+			}
+			api.ackL.RUnlock()
+			time.Sleep(5 * time.Millisecond)
+		}
+	}()
+	select {
+	case <-time.After(30 * time.Second):
+		return false, errors.New("ACK timeout")
+	case rVal := <-vFound:
+		return rVal, vFerr
+
+	}
+}
+
+func (api *APIClient) formatURL(apiType string, methodEndpoint string) string {
+	protocol := "https"
+	if apiType == "realtime" {
+		protocol = "wss"
+	}
+	return fmt.Sprintf("%s://%s.%s/%s/%s", protocol, apiType, api.Endpoint, apiVersion, methodEndpoint)
+}
+
+//Staging switches to the staging env endpoints
+func (api *APIClient) Staging() {
+	api.Endpoint = stagingAPIEndpoint
+}
+
+func (api *APIClient) getSocket() *websocket.Conn {
+	return api.socket
+}
---
+++ external/go-evntsrc/pubsub.go
@@ -0,0 +1,277 @@
+package evntsrc
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tcfw/evntsrc/pkg/websocks"
+
+	"github.com/gorilla/websocket"
+)
+
+func (api *APIClient) openConn() error {
+	var headers *http.Header
+	if api.auth != "" {
+		headers = &http.Header{}
+		headers.Add("Authorization", fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(api.auth))))
+	}
+
+	url := api.formatURL("realtime", strconv.Itoa(int(api.stream)))
+
+	conn, resp, err := websocket.DefaultDialer.Dial(url, *headers)
+	conn.SetCloseHandler(func(code int, text string) error {
+		fmt.Printf("CLOSED %v %v\n", code, text)
+		api.close <- true
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("%v\n", resp)
+		return err
+	}
+
+	api.socket = conn
+
+	go api.readPump()
+	go api.writePump()
+
+	return nil
+}
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 2 * time.Minute
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+)
+
+func (api *APIClient) readPump() {
+	api.socket.SetReadLimit(512)
+	api.socket.SetPongHandler(func(string) error {
+		fmt.Println("handled pong")
+		return nil
+	})
+
+	defer func() {
+		api.socket.Close()
+		api.socket = nil
+		api.close <- true
+	}()
+
+	go api.distributeReadPipe()
+
+	for {
+		_, message, err := api.socket.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				fmt.Printf("ERR Reading: %v\n", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, []byte{'\n'}, []byte{' '}, -1))
+
+		if strings.HasPrefix(string(message), `{"acktype"`) {
+			command := &websocks.AckCommand{}
+			if err = json.Unmarshal(message, command); err != nil {
+				errMsg := &websocks.AckCommand{Acktype: "Error", Error: "Failed to parse command"}
+				jsonBytes, _ := json.Marshal(errMsg)
+				api.Errors <- errors.New(string(jsonBytes))
+			}
+
+			api.AcksCh <- command
+			continue
+		}
+		if strings.Contains(string(message), `connectionID`) {
+			info := &websocks.ConnectionInfo{}
+			if err = json.Unmarshal(message, info); err != nil {
+				errMsg := &websocks.AckCommand{Acktype: "Error", Error: "Failed to parse connection info"}
+				jsonBytes, _ := json.Marshal(errMsg)
+				api.Errors <- errors.New(string(jsonBytes))
+				return
+			}
+			api.connectionID = info.ConnectionID
+			continue
+		}
+
+		api.ReadPipe <- message
+	}
+}
+
+func (api *APIClient) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+	}()
+
+	for {
+		select {
+		case msg := <-api.writePipe:
+			if err := api.doPublish(msg); err != nil {
+				api.Errors <- err
+			}
+		case <-api.close:
+			return
+		case <-ticker.C:
+			if err := api.socket.WriteMessage(websocket.PingMessage, nil); err != nil {
+				api.Errors <- fmt.Errorf("Failed to send ping")
+			}
+		}
+	}
+}
+
+func (api *APIClient) distributeReadPipe() {
+	for {
+		msg, ok := <-api.ReadPipe
+		if !ok {
+			//Read pipe is closed
+			break
+		}
+
+		evnt := &Event{}
+		if err := json.Unmarshal(msg, evnt); err != nil {
+			api.Errors <- err
+			continue
+		}
+
+		if source, ok := evnt.Metadata["relative_seq"]; api.IgnoreSelf && ok && strings.HasPrefix(source, fmt.Sprintf("%s-", api.connectionID)) {
+			continue
+		}
+
+		go func(evnt *Event) {
+			for subject := range api.subscriptions {
+				if subject == evnt.Subject {
+					for _, subset := range api.subscriptions[subject] {
+						switch subset.subType {
+						case funcSubType:
+							subset.f(evnt)
+							break
+						case chanSubType:
+							subset.ch <- evnt
+							break
+						}
+					}
+				}
+			}
+		}(evnt)
+	}
+}
+
+func (api *APIClient) doPublish(data *websocks.PublishCommand) error {
+	if api.socket == nil {
+		if err := api.openConn(); err != nil {
+			return fmt.Errorf("Failed to publish: %s", err.Error())
+		}
+	}
+
+	return api.socket.WriteJSON(data)
+}
+
+//Publish publishes an event through evntsrc
+func (api *APIClient) Publish(subject string, data []byte, eventType string) error {
+	pubMsg := &websocks.PublishCommand{
+		SubscribeCommand: &websocks.SubscribeCommand{InboundCommand: &websocks.InboundCommand{Ref: uuid.New().String(), Command: "pub"}, Subject: subject},
+		Data:             base64.StdEncoding.EncodeToString(data),
+		ContentType:      "application/json",
+		Type:             eventType,
+		TypeVersion:      api.AppVer,
+	}
+
+	api.writePipe <- pubMsg
+	ok, err := api.waitForResponse(pubMsg.Ref)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	if !ok {
+		return fmt.Errorf("Failed to publish: %s", err.Error())
+	}
+
+	return err
+}
+
+func (api *APIClient) doSubscribe(subject string) error {
+	if api.socket == nil {
+		if err := api.openConn(); err != nil {
+			return fmt.Errorf("Failed to subscribe: %s", err.Error())
+		}
+	}
+
+	subMsg := &websocks.SubscribeCommand{
+		InboundCommand: &websocks.InboundCommand{Ref: uuid.New().String(), Command: "sub"},
+		Subject:        subject,
+	}
+
+	if err := api.socket.WriteJSON(subMsg); err != nil {
+		return err
+	}
+
+	_, err := api.waitForResponse(subMsg.Ref)
+	if err != nil {
+		return fmt.Errorf("Failed to subscribe: %s", err.Error())
+	}
+
+	return nil
+}
+
+//Subscribe to a subject inside the stream via a channel
+func (api *APIClient) Subscribe(subject string) (chan *Event, error) {
+	if err := api.doSubscribe(subject); err != nil {
+		return nil, err
+	}
+
+	ch := make(chan *Event, 256)
+
+	if _, ok := api.subscriptions[subject]; !ok {
+		api.subscriptions[subject] = []*subscription{}
+	}
+
+	api.subscriptions[subject] = append(api.subscriptions[subject], &subscription{subType: chanSubType, ch: ch})
+
+	return ch, nil
+}
+
+//SubscribeFunc subscribes to a subject and call callback func
+func (api *APIClient) SubscribeFunc(subject string, callback func(*Event)) error {
+	if err := api.doSubscribe(subject); err != nil {
+		return err
+	}
+
+	if _, ok := api.subscriptions[subject]; !ok {
+		api.subscriptions[subject] = []*subscription{}
+	}
+
+	api.subscriptions[subject] = append(api.subscriptions[subject], &subscription{subType: funcSubType, f: callback})
+
+	return nil
+}
+
+//Unsubscribe from a subject
+func (api *APIClient) Unsubscribe(subject string) error {
+	if subset, ok := api.subscriptions[subject]; ok {
+		for _, subscription := range subset {
+			switch subscription.subType {
+			case chanSubType:
+				close(subscription.ch)
+				break
+			case funcSubType:
+				//nothing to do
+				break
+			}
+		}
+
+		delete(api.subscriptions, subject)
+		return nil
+	}
+
+	return fmt.Errorf("No subscription for subject '%s'", subject)
+}

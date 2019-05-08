e53c54bc5b3526db4f088b763d1e93b261b0ef67
Tom Worrall <tom@hostngo.com.au>
2019-05-08 16:47:50 +1000

    script to build changed services only

---
+++ scripts/changes_services.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+detect_changed_services() {
+	echo "----------------------------------------------"
+ 	echo "detecting changed folders for this commit"
+
+	# get a list of all the changed folders only
+	changed_folders=`git diff --name-only $COMMIT_RANGE | grep internal/ | awk 'BEGIN {FS="/"} {print $2}' | uniq`
+	echo "changed folders "$changed_folders
+
+	changed_services=()
+	for folder in $changed_folders
+	do
+		if [ "$folder" == 'utils' ] || [ "$folder" == 'tracing' ]; then
+			echo "!! a common folder changed, building and publishing all microservices"
+			changed_services=`find ./internal -maxdepth 1 -type d -not -name 'utils' -not -name 'tracing' -not -name '.git' -not -path './internal' | sed 's|./internal/||'`
+			break
+		else
+			echo "Adding $folder to list of services to build"
+			changed_services+=("$folder")
+		fi
+	done
+
+	# Iterate on each service and run the packaging script
+	for service in ${changed_services[@]}
+	do
+		echo ""
+		echo "-------------------Running packaging for $service---------------------"
+		make $service
+	done
+}
+
+detect_changed_services

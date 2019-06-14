1c900885dcfab976f0e59db695a704be0af18178
Tom Worrall <tom@evntsrc.io>
2019-06-14 19:15:00 +1000

    WIP scheduler deployment + stream list

--- internal/ttlscheduler/streams.go
+++ internal/ttlscheduler/streams.go
@@ -1,11 +1,40 @@
 package ttlscheduler
 
 import (
+	"context"
+	"time"
+
+	streamsPb "github.com/tcfw/evntsrc/internal/streams/protos"
 	pb "github.com/tcfw/evntsrc/internal/ttlscheduler/protos"
+	"google.golang.org/grpc"
+	grpcRoundRobin "google.golang.org/grpc/balancer/roundrobin"
 )
 
-type basicStreamFetcher struct{}
+type basicStreamFetcher struct {
+	streamsConn streamsPb.StreamsServiceClient
+}
 
 func (bsf *basicStreamFetcher) GetStreams() ([]*pb.Stream, error) {
-	return []*pb.Stream{}, nil
+	if bsf.streamsConn == nil {
+		conn, err := grpc.Dial("dns:///streams:443", grpc.WithInsecure(), grpc.WithBalancerName(grpcRoundRobin.Name))
+		if err != nil {
+			return nil, err
+		}
+
+		bsf.streamsConn = streamsPb.NewStreamsServiceClient(conn)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	ids, err := bsf.streamsConn.ListIds(ctx, &streamsPb.SearchRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	list := []*pb.Stream{}
+	for _, id := range ids.ID {
+		list = append(list, &pb.Stream{Id: id, MsgRate: 0})
+	}
+
+	return list, nil
 }

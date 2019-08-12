3d8f91bd8753cdd7ca41a2a372d84795380ae7dd
Tom Worrall <tom@evntsrc.io>
2019-08-13 08:28:16 +1000

    feat: allow perminant deletion of events

--- internal/storer/server.go
+++ internal/storer/server.go
@@ -19,7 +19,6 @@ type server struct {
 
 //newServer adds connection to the streams service
 func newServer() (*server, error) {
-
 	conn, err := grpc.Dial("dns:///streams:443", grpc.WithInsecure(), grpc.WithBalancerName(grpcRoundRobin.Name))
 	if err != nil {
 		return nil, err
@@ -30,6 +29,7 @@ func newServer() (*server, error) {
 	return &server{streamsConn}, err
 }
 
+//Acknowledges appends a timestamp to the event
 func (s *server) Acknowledge(ctx context.Context, req *pb.AcknowledgeRequest) (*pb.AcknowledgeResponse, error) {
 	if err := s.validateStreamOwnership(ctx, req); err != nil {
 		return nil, err
@@ -42,9 +42,13 @@ func (s *server) Acknowledge(ctx context.Context, req *pb.AcknowledgeRequest) (*
 
 	return &pb.AcknowledgeResponse{Time: ackTime}, nil
 }
+
+//ExtendTTL GRPC alias
 func (s *server) ExtendTTL(ctx context.Context, req *pb.ExtendTTLRequest) (*pb.ExtendTTLResponse, error) {
 	return &pb.ExtendTTLResponse{}, extendTTL(req)
 }
+
+//Query facilitates differing types of TTL queries
 func (s *server) Query(req *pb.QueryRequest, stream pb.StorerService_QueryServer) error {
 	switch qType := req.Query.(type) {
 	case *pb.QueryRequest_Ttl:
@@ -54,7 +58,12 @@ func (s *server) Query(req *pb.QueryRequest, stream pb.StorerService_QueryServer
 	}
 }
 
+//ReplayEvent queries DB for single event and publishes to NATS
 func (s *server) ReplayEvent(ctx context.Context, req *pb.ReplayEventRequest) (*pb.ReplayEventResponse, error) {
+	if err := s.validateStreamOwnership(ctx, req); err != nil {
+		return nil, err
+	}
+
 	rQ, err := pgdb.Query(`SELECT * FROM event_store.events WHERE stream = $1 AND id = $2 LIMIT 1`, req.GetStream(), req.GetEventID())
 	if err != nil {
 		return nil, fmt.Errorf("sqlc: %s", err.Error())
@@ -79,10 +88,21 @@ func (s *server) ReplayEvent(ctx context.Context, req *pb.ReplayEventRequest) (*
 	return &pb.ReplayEventResponse{}, nil
 }
 
+//Store RPC storage request similar to NATS subscription
 func (s *server) Store(ctx context.Context, req *pb.StoreRequest) (*pb.StoreResponse, error) {
 	err := storeEvent(req.Event, pgdb)
 
 	return &pb.StoreResponse{}, err
+}
+
+func (s *server) Delete(ctx context.Context, req *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	if err := s.validateStreamOwnership(ctx, req); err != nil {
+		return nil, err
+	}
+
+	_, err := pgdb.ExecContext(ctx, `DELETE FROM event_store.events WHERE stream = $1 AND id = $2 LIMIT 1`, req.GetStream(), req.GetEventID())
+
+	return &pb.DeleteResponse{}, err
 }
 
 type streamedRequest interface {
